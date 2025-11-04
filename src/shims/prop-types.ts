641443d9833bb1646c28372f4edd4eac7bd07c1b
jude1211 <judinmathew2002@gmail.com>
2025-11-04 20:11:45 +0530

    env

--- src/shims/prop-types.ts
+++
@@ -1,7 +0,0 @@
-// Minimal no-op shim for prop-types to satisfy legacy libs in dev
-const PropTypes: Record<string, any> = new Proxy({}, {
-  get: () => () => null,
-});
-
-export default PropTypes;
-
